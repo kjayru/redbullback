c27e4646ebd73392eecae488729dd67435beb22f
kjayru <wiltinoco@gmail.com>
2024-09-13 19:22:48 -0500

    fix

--- app/Models/Registro.php
+++ app/Models/Registro.php
@@ -10,4 +10,13 @@ class Registro extends Model
     use HasFactory;
     protected $table="registros";
 
+    protected $fillable = [
+        'nombres_y_apellidos',
+        'codigo_c',
+        'subir_imagen',
+        'ciudad_donde_trabajas',
+        'region_donde_trabajas',
+        'acepto_los_terminos_y_condiciones',
+        'autorizo_el_tratamiento_de_mis_datos_personales_e_imagen',
+    ];
 }
